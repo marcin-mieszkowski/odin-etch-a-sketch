ebb38f2f34e6f55342d44f398ba94e69a598a477
Marcin Mieszkowski <96139086+marcin-mieszkowski@users.noreply.github.com>
2024-10-23 13:59:12 +0200

    feat: add basic structure for Etch-A-Sketch project
    - Add index.html with HTML structure
    - Create style.css for layout and grid styling
    - Add script.js to handle grid setup logic

---
+++ script.js
@@ -0,0 +1,7 @@
+const gridContainer = document.querySelector(".grid-container");
+const gridButton = document.querySelector(".grid-button");
+const grid = document.querySelector(".grid");
+
+let gridSize = 16;
+
+
